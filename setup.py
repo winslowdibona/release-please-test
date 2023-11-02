96e9ac3c5fe0209afaf7ade5fdd454f931804a25
winslowdibona <dibona.winslow@gmail.com>
2023-11-02 09:20:10 -0400

    chore: add boilerplate files

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='release-please-test',
+    version='0.0.1',
+    author='winslowdibona',
+    packages=find_packages(exclude=['release-please-test/tests']),
+    url='https://github.com/winslowdibona/release-please-test',
+    license='LICENSE',
+    description='',
+    long_description=open('README.md').read(),
+    install_requires=[
+        "Django == 3.2.22",
+    ],
+)
